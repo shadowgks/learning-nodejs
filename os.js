31f01f184f745d080600a9b6d067d11f1d678fb2
shadowgks <saad.moumou.coder@gmail.com>
2023-05-08 14:57:21 +0100

    OS module done

---
+++ os.js
@@ -0,0 +1,8 @@
+const { log } = require('console');
+const os = require('os');
+
+const free_mem = os.freemem();
+const total_mem = os.totalmem();
+
+console.log(`Free Mem: ${free_mem}`);
+console.log(`Total Mem: ${total_mem}`);
